e974576ad9ca373afd688c95fa3436c0629fda47
zeurese.bustamante <eurese.bustamante@orangeandbronze.com>
2026-03-20 14:50:47 +0800

    Initial Commit

---
+++ KeepingUp/KeepingUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepingUpApp.swift
+//  KeepingUp
+//
+//  Created by EureseB on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct KeepingUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeepingUpTests/KeepingUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeepingUpTests.swift
+//  KeepingUpTests
+//
+//  Created by EureseB on 3/20/26.
+//
+
+import Testing
+@testable import KeepingUp
+
+struct KeepingUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
